22a4e8b2f0db76f882603b4ee6b37aaf812f8d01
Anil <105417410+CarefreeAnil@users.noreply.github.com>
2026-02-20 18:32:29 +0200

    Updated allowedhosts

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,15 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import { componentTagger } from "lovable-tagger";
 import { viteStaticCopy } from "vite-plugin-static-copy";
+import componentTagger from "vite-plugin-component-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
     port: 8080,
+    allowedHosts: ["ifcgraphviewer.onrender.com"],
   },
   plugins: [
     react(),
